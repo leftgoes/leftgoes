5cb3a3a90453ee6cfe96a82ef539c567be95964b
Ha Jong Kim <93378425+leftgoes@users.noreply.github.com>
2022-10-12 06:28:26 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,44 @@
+import numpy as np
+from typing import Callable
+from lmfit import Model
+
+T = float | np.ndarray
+
+class LMFit:
+    __slots__ = ('f', 'parameters')
+
+    def __init__(self, f: Callable[[T], T]):
+        self.f = f
+        self.parameters: dict[str, tuple[float, float]] = {}
+
+    def __getattr__(self, variable: str) -> float:
+        return self.parameters[variable]
+
+    @property
+    def args(self) -> tuple[str]:
+        return self.f.__code__.co_varnames[1:]  # assuming first argument is x
+
+    def fit(self, x: list[float], y: list[float], **kws) -> None:
+        model = Model(self.f)
+        initial = {arg: 1 for arg in self.args}
+        initial.update(kws)
+        result = model.fit(y, x=x, **initial)
+        self.parameters.update({p: (v.value, v.stderr) for p, v in result.params.items()})
+
+    def get(self, start: float, end: float, n: int = 300) -> tuple[np.ndarray, np.ndarray]:
+        x = np.linspace(start, end, n)
+        y = self.f(x, **{p: v[0] for p, v in self.parameters.items()})
+        return x, y
+
+def linmap(x: T, from_range: tuple[T, T], to_range: tuple[T, T]) -> T:
+    return to_range[0] + (x - from_range[0]) * (to_range[1] - to_range[0]) / (from_range[1] - from_range[0])
+
+def expmap(x: T, from_range: tuple[T, T], to_range: tuple[T, T], base: float = np.e) -> T:
+    return (base**(x - from_range[0]) - 1)/(base**(from_range[1] - from_range[0]) - 1) * (to_range[1] - to_range[0]) + to_range[0]
+
+def progressbar(bar_length: int = 30) -> Callable[[float], str]:
+    def f(progress: float):
+        quarters = '_░▒▓█'
+        done = int(progress * bar_length)
+        return (done * '█' + quarters[round(4 * (bar_length * progress - done))] + int((1 - progress) * bar_length) * '_')[:bar_length]
+    return f
